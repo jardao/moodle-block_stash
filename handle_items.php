93cc990b2e079d3de497af305103e2824dcd6b71
Andres MF <munozfernandezandres@gmail.com>
2018-06-15 11:16:38 +0200

    procesado form handle_items items ya en posesión

--- handle_items.php
+++ handle_items.php
@@ -4,6 +4,9 @@ require_once(__DIR__ . '/../../config.php');
 require_once('./classes/form/handleitem_form.php');
 
 global $PAGE;
+global $DB;
+
+use coding_exception;
 
 $courseid = required_param('courseid', PARAM_INT);
 $userid = required_param('userid', PARAM_INT);
@@ -17,7 +20,6 @@ $manager->require_manage();
 $context = context_course::instance($courseid);
 $url = new moodle_url('/blocks/stash/handle_items.php', array('userid' => $userid, 'courseid' => $courseid));
 
-//estas lineas las extraigo del page_helper.php
 $title = 'Report';
 $subtitle = 'Handling items';
 
@@ -27,17 +29,46 @@ $PAGE->set_title($title);
 $PAGE->set_heading($manager->get_context()->get_context_name());
 $PAGE->set_url($url);
 
-$returnurl = new moodle_url('/blocks/stash/report.php', ['courseid' => $manager->get_courseid()]);
+$returnurl = new moodle_url('/blocks/stash/report.php', ['courseid' => $courseid]);
 
 $PAGE->navbar->add(get_string('stash', 'block_stash'));
 $PAGE->navbar->add($title, $returnurl);
 $PAGE->navbar->add($subtitle);
 
 $renderer = $PAGE->get_renderer('block_stash');
-//$form = new \block_stash\form\handleitem_form();
 $form = new handleitem_form(null, array('courseid' => $courseid, 'userid' => $userid));
 
 if($data = $form->get_data()){
+
+	$courseid = $data -> courseid;
+	$userid = $data -> userid;
+	$itemid = $data -> itemid;
+	$itemquantity = $data -> itemquantity;
+
+	//to check courseid
+	$form_manager = \block_stash\manager::get($courseid);
+
+	//to check userid
+	$form_manager -> require_acquire_items($userid);
+
+	//to check wehter the item belongs to the stash course
+	$item_is_in_stash = \block_stash\item::is_item_in_stash($itemid, $form_manager->get_stash()->get_id());
+    if (!$item_is_in_stash ) {
+
+    	throw new coding_exception("Invalid item");
+	} 
+
+    //to check itemquantity
+    if($itemquantity < 1){
+
+    	throw new coding_exception("Invalid item quantity");
+    }
+
+    //we check wether the user already has the item
+    if($DB -> record_exists('block_stash_user_items', array('itemid' => $itemid, 'userid' => $userid))){
+
+    	$DB -> set_field('block_stash_user_items', 'quantity', $itemquantity, array('itemid' => $itemid, 'userid' => $userid));
+    }
 
 	redirect($returnurl);
 
